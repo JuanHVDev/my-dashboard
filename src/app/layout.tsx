4b293c9ce529e6bc3bdef32ba6116e0193f0ad2b
Juan Herrera Vázquez <jherrerav1901@alumno.ipn.mx>
2023-12-23 23:51:21 -0600

    Proyecto finalizado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,9 +13,10 @@ export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
-}) {
+})
+{
   return (
-    <html lang="en">
+    <html lang="es">
       <body className={inter.className}>{children}</body>
     </html>
   )
